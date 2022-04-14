19e3423acf141332231ae267410cd9bcb046915d
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-04-14 11:09:15 -0500

    :wrench: add css tailwindcss configuration

---
+++ dts.config.js
@@ -0,0 +1,24 @@
+const postcss = require('rollup-plugin-postcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  rollup(config, options) {
+    config.plugins.push(
+      postcss({
+        plugins: [
+          autoprefixer(),
+        ],
+        config: {
+          path: "./postcss.config.js",
+        },
+        extensions: [".css"],
+        minimize: true,
+        output: "./dist/custom.css",
+        inject: false,
+        // only write out CSS for the first bundle (avoids pointless extra files):
+        extract: !!options.writeMeta,
+      })
+    );
+    return config;
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
