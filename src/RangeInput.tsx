439a9c896b85eb5319278edcac57a3418f897dbc
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-04-20 15:57:50 -0500

    :recycle: :notebook: add inline jsdoc docs and optional rangeVal input #1

--- src/RangeInput.tsx
+++ src/RangeInput.tsx
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 
-export default function RangeInput() {
-  const [rangeval, setRangeval] = useState('250');
+/**
+ * @description
+ * The ad budget slider component. Input is required by the user though
+ * has a default selection of 250.
+ *
+ * > Refer to this input by its id `budget`.
+ *
+ * @param rangeVal - the default value for user selection between $50 and $10,000, inclusive.
+ */
+export default function RangeInput({ rangeVal }: { rangeVal?: number }) {
+  const [rangeval, setRangeval] = useState(
+    !!rangeVal && rangeVal > 49.99 && rangeVal < 10000.01
+      ? `${rangeVal}`
+      : '250'
+  );
   const minSpend = 50;
   const maxSpend = 10000;
   return (
