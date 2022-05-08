cc8130e8f08f21b198e63e81d611f040ff796ba0
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-05-08 17:09:06 -0500

    :recycle: cleanup css imports for button locally

--- example/nextjs/pages/_app.tsx
+++ example/nextjs/pages/_app.tsx
@@ -1,6 +1,5 @@
-import '../../../dist/bundle.css'; // @tincre/promo-button/bundle.css
-
 import type { AppProps } from 'next/app';
+import '../../../dist/promo-button.esm.css'; // @tincre/promo-button/bundle.css
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
