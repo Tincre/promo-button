058929ae61abfaf5e18e263e8027082d3e39a3e9
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-07-24 19:44:33 -0500

    :sparkles: add Icon components for links per #29

--- src/icons.tsx
+++ src/icons.tsx
@@ -6,6 +6,16 @@
  */
 
 import React from 'react';
+import facebook from "super-tiny-icons/images/svg/facebook.svg";
+import twitter from 'super-tiny-icons/images/svg/twitter.svg';
+import linkedin from 'super-tiny-icons/images/svg/linkedin.svg';
+import spotify from 'super-tiny-icons/images/svg/spotify.svg';
+import github from 'super-tiny-icons/images/svg/github.svg';
+import instagram from 'super-tiny-icons/images/svg/instagram.svg';
+import snapchat from 'super-tiny-icons/images/svg/snapchat.svg';
+import apple from 'super-tiny-icons/images/svg/apple.svg';
+import youtube from 'super-tiny-icons/images/svg/youtube.svg';
+import internet from 'super-tiny-icons/images/svg/wifi.svg';
 export function XIcon({ className }: { className: string }) {
   return (
     <svg
@@ -24,3 +34,44 @@ export function XIcon({ className }: { className: string }) {
     </svg>
   );
 }
+
+function BaseIcon({src, className, width, height}: {src: string; className: string; width?: string | number | undefined; height?: string | number | undefined}) {
+  return <img src={src} height={height || 25} width={width || 25} className={className} />
+}
+export function FacebookIcon({className, }: {className: string}) {
+  return <BaseIcon src={facebook} className={className} />
+}
+
+export function TwitterIcon({className, }: {className: string}) {
+  return <BaseIcon src={twitter} className={className} />
+}
+
+export function LinkedInIcon({className, }: {className: string}) {
+  return <BaseIcon src={linkedin} className={className} />
+}
+
+
+export function SpotifyIcon({className, }: {className: string}) {
+  return <BaseIcon src={spotify} className={className} />
+}
+export function GithubIcon({className, }: {className: string}) {
+  return <BaseIcon src={github} className={className} />
+}
+export function InstagramIcon({className, }: {className: string}) {
+  return <BaseIcon src={instagram} className={className} />
+}
+export function SnapchatIcon({className, }: {className: string}) {
+  return <BaseIcon src={snapchat} className={className} />
+}
+export function AppleIcon({className, }: {className: string}) {
+  return <BaseIcon src={apple} className={className} />
+}
+
+export function YoutubeIcon({className, }: {className: string}) {
+  return <BaseIcon src={youtube} className={className} />
+}
+export function InternetIcon({className, }: {className: string}) {
+  return <BaseIcon src={internet} className={className} />
+}
+
+
