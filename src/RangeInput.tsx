c84cb585dfd32ac42dd41181c45520a65eff1a13
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-07-27 13:04:21 -0500

    :sparkles: add promo-button-range-input* classes #38

--- src/RangeInput.tsx
+++ src/RangeInput.tsx
@@ -28,13 +28,13 @@ export default function RangeInput({ rangeVal }: { rangeVal?: number }) {
     <div className="mt-3">
       <label
         htmlFor="budget"
-        className="block text-sm font-medium text-gray-700"
+        className="promo-button-range-input-label"
       >
         {`Budget: $${rangeval}`}
       </label>
-      <div className="mt-1 rounded-md">
+      <div className="promo-button-range-input-container">
         <input
-          className="block w-full pl-16 sm:pl-14 sm:text-sm border-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-700 text-black focus:ring-border-black hover:border-transparent rounded-md"
+          className="promo-button-range-input"
           type="range"
           name="budget"
           id="budget"
