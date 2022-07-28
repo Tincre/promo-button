76034386311b06894ddbaaae8534937217f306ab
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-07-28 11:13:10 -0500

    :recycle: refactor to extract SubmitButton into modular component

--- src/index.tsx
+++ src/index.tsx
@@ -18,11 +18,10 @@ import ThreeWords from './ThreeWords';
 import { useScript } from './lib/useScript';
 import { getToken } from './lib/getToken';
 import HowItWorks from './HowItWorks';
-import TeamEmail from './TeamEmail';
+import SubmitButton from './SubmitButton';
+//import TeamEmail from './TeamEmail';
 
 const Cloudinary = React.lazy(() => import('./Cloudinary'));
-
-
 
 export function ImageUploadError() {
   return (
@@ -234,42 +233,10 @@ export function PromoButton({
                                 <ImageUploadError />
                               ) : null}
                               {targetLinkError ? <TargetLinkError /> : null}
-                              <button
-                                type="submit"
-                                disabled={isSubmitting && fileImage}
-                                className={`${
-                                  !isSubmitting
-                                    ? ''
-                                    : 'promo-button-dialog-submission-button-disabled'
-                                } promo-button-dialog-submission-button`}
-                              >
-                                {!isSubmitting ? (
-                                  <svg
-                                    className="hidden -ml-1 mr-3 h-5 w-5 text-white"
-                                    viewBox="0 0 24 24"
-                                  />
-                                ) : (
-                                  <svg
-                                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-900"
-                                    viewBox="0 0 24 24"
-                                  >
-                                    <circle
-                                      className="opacity-25"
-                                      cx="12"
-                                      cy="12"
-                                      r="10"
-                                      stroke="currentColor"
-                                      strokeWidth="4"
-                                    ></circle>
-                                    <path
-                                      className="opacity-75"
-                                      fill="currentColor"
-                                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                    ></path>
-                                  </svg>
-                                )}
-                                Submit
-                              </button>
+                              <SubmitButton
+                                isSubmitting={isSubmitting}
+                                fileImage={fileImage}
+                              />
                             </div>
                           </div>
                         ) : (
