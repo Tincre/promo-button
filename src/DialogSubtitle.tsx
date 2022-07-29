09ccdaca63f2891c55f1d335e9daf20feb70d06f
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-07-29 11:28:07 -0500

    :recycle: refactor to use promo prefix for tailwindcss classes #46

--- src/DialogSubtitle.tsx
+++ src/DialogSubtitle.tsx
@@ -1,6 +1,6 @@
 export default function DialogSubtitle({ isSubmitted }: { isSubmitted: any }) {
   return (
-    <div className="mt-2">
+    <div className="promo-mt-2">
       <p className="promo-button-dialog-subtitle-text">
         {!isSubmitted
           ? `We need just a few items to start.`
