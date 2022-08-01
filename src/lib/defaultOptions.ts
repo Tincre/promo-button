a185fee87e28a96893e54c71d5188cfbb5a2d140
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-08-01 15:03:06 -0500

    :sparkles: :wrench: add defaultOptions module per #58

---
+++ src/lib/defaultOptions.ts
@@ -0,0 +1,17 @@
+const options: {
+  cloudinary: {
+    cloudName: string;
+    uploadPreset: string;
+    folder: string;
+    multiple: boolean;
+  }
+}= {
+  cloudinary: {
+    cloudName: 'b00st',
+    uploadPreset: 'uscb5ifq',
+    folder: 'B00STButton',
+    multiple: true,
+  },
+};
+
+export default options;
