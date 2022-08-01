5e7ed71147742ba4505fe62215e0629b36917d3e
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-08-01 15:24:48 -0500

    :recycle: refactor to take configurable user options

--- src/Cloudinary.tsx
+++ src/Cloudinary.tsx
@@ -6,8 +6,12 @@
  */
 
 import { detectMediaType, getOptions } from './lib/utils';
+import { CloudinaryOptions } from './lib/defaultOptions';
 
-function getWidget(setFileImage: any, cloudinaryOptions?: undefined | object) {
+function getWidget(
+  setFileImage: any,
+  cloudinaryOptions?: undefined | CloudinaryOptions
+) {
   if (typeof window.cloudinary === 'undefined') return null;
   let options = getOptions(cloudinaryOptions);
   console.debug(`getCloudinary options set: ${JSON.stringify(options)}`);
@@ -32,14 +36,16 @@ function showWidget(widget: any) {
 export default function Cloudinary({
   imageData,
   setFileImage,
+  options,
 }: {
   imageData: any;
   setFileImage: any;
+  options?: undefined | CloudinaryOptions;
 }) {
   // TODO add func input for alt text (should take copy generated for ad)
   const mediaUrlType = detectMediaType(imageData?.secure_url);
   // TODO render <video> or <img> as appropriate
-  let widget = getWidget(setFileImage);
+  let widget = getWidget(setFileImage, options);
   return (
     <>
       <div className="promo-mt-3" id="cloudinary-upload-widget">
